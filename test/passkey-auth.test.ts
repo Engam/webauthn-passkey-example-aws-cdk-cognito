1f6c65d8e601940f55315ac0247b075cf8639661
Engam <magne.garaas@icloud.com>
2024-10-05 15:16:44 +0200

    Initial commit

---
+++ test/passkey-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PasskeyAuth from '../lib/passkey-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/passkey-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PasskeyAuth.PasskeyAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
